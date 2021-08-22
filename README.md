6c58869005af75ac35c4d6406a0f58dfe2b0e7ab
Chen BAI <baichen318@gmail.com>
2021-08-22 23:05:25 +0800

    Update auto VLSI

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ $ python main.py -c configs/rl-explorer.yml
 ```
 
 ## TODO
-- Sample:
-    * Sample batch configs.
+- VLSI:
+	* Log each running compilation
+	* Re-run when compilation occurs errors
+	* auto-vlsi.sh needs to handle error compilations
 - Invalid configs.:
     * Assign a large negtive reward
--- vlsi/scripts/generate-auto-vlsi.sh
+++ vlsi/scripts/generate-auto-vlsi.sh
@@ -27,16 +27,16 @@ do
     echo compiling \${idx}-th Config.
     soc_name=Boom\${idx}Config
     make MACROCOMPILER_MODE='-l /research/dept8/gds/cbai/research/chipyard/vlsi/hammer/src/hammer-vlsi/technology/asap7/sram-cache.json' CONFIG=\${soc_name} &
-    # 220 sec. would be suitable
-    sleep 220
+    # 200 sec. would be suitable
+    sleep 200
 done
 
 # verify all simv have been generated
-count=\`expr \${end} - \${start} + 1\`
+count=\`expr ${end} - ${start} + 1\`
 c=0
 while [[ \${c} -lt \${count} ]]
 do
-    for idx in \${arr[@]}
+    for idx in \`seq ${start} ${end}\`
     do
         soc_name=Boom\${idx}Config
         if [[ -e simv-chipyard-\${soc_name} ]]
@@ -55,7 +55,7 @@ do
 done
 
 # simulate
-for idx in \${arr[@]}
+for idx in \`seq ${start} ${end}\`
 do
     soc_name=Boom\${idx}Config
     if [[ -e \${soc_name}/simv-chipyard-\${soc_name} ]]
