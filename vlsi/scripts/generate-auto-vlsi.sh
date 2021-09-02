52fe38d7dd5386ea0f28c1288a15b9128b261c43
Chen BAI <baichen318@gmail.com>
2021-09-02 23:49:49 +0800

    Update auto-vlsi

--- vlsi/scripts/generate-auto-vlsi.sh
+++ vlsi/scripts/generate-auto-vlsi.sh
@@ -27,8 +27,8 @@ do
     echo compiling \${idx}-th Config.
     soc_name=Boom\${idx}Config
     make -j80 MACROCOMPILER_MODE='-l /research/dept8/gds/cbai/research/chipyard/vlsi/hammer/src/hammer-vlsi/technology/asap7/sram-cache.json' CONFIG=\${soc_name} &
-    # 45 sec. would be suitable
-    sleep 45
+    # 75 sec. would be suitable
+    sleep 75
 done
 
 # verify all simv have been generated
