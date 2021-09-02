2cd8a9f8733d48508be7b816b6f247b536bfc323
Chen BAI <baichen318@gmail.com>
2021-09-02 10:12:45 +0800

    Add parts of Rocket code

--- dse/env/rocket/design_env.py
+++ dse/env/rocket/design_env.py
@@ -14,11 +14,11 @@ class BasicEnv(object):
         super(BasicEnv, self).__init__()
         self.configs = configs
 
-class RocketDesignEnv(BasicEnv):
-    """ RocketDesignEnv """
+class BoomDesignEnv(BasicEnv):
+    """ BoomDesignEnv """
     # def __init__(self, configs, seed=int(time.time())):
     def __init__(self, configs, seed=2021):
-        super(RocketDesignEnv, self).__init__(configs)
+        super(BoomDesignEnv, self).__init__(configs)
         self.design_space = parse_design_space(
             self.configs["design-space"],
             basic_component=self.configs["basic-component"],
@@ -69,6 +69,7 @@ class RocketDesignEnv(BasicEnv):
         for i in range(self.configs["batch"]):
             if not self.design_space.validate(self.state[i].numpy()):
                 # no transition
+                self.configs["logger"].info("[INFO]: %s is invalid." % self.state[i].numpy())
                 dont_vlsi[i] = 1
                 reward[i] = 0
         # `valid_idx`: <torch.Tensor>
@@ -83,24 +84,27 @@ class RocketDesignEnv(BasicEnv):
             )
             k = 0
             for i in vlsi_idx:
-                reward[i] = ipc[k] - self.init_ipc[i]
+                reward[i] = ipc[k]
                 # update `self.best_ipc`
-                if reward[i] > 0:
-                    self.best_ipc[i] = ipc[k]
+                if reward[i] > self.ipc[i]:
+                    self.ipc[i] = ipc[k]
                 k += 1
         reward = torch.Tensor(reward)
-        msg = "[INFO]: reward: %s" % reward
+        msg = "[INFO]: state: %s, reward: %s, current best ipc: %s" % (self.state.numpy(), reward, self.ipc)
         self.configs["logger"].info(msg)
-        if not torch.equal(
-            self.best_idx,
-            torch.where(self.best_ipc == max(self.best_ipc))[0]
+        if bool(torch.lt(
+                self.best_ipc,
+                torch.max(self.ipc)
+            )
         ):
-            self.best_idx = torch.where(self.best_ipc == max(self.best_ipc))[0]
+            # the best IPC is increased
+            self.best_ipc = torch.max(self.ipc)
+            best_idx = torch.where(self.ipc == self.best_ipc)[0]
             self.last_update = self.n_step
-            msg = "[INFO]: current state: %s, best state: %s, best ipc: %.8f" % (
+            msg = "[INFO]: current state: %s, global best state: %s, global best ipc: %.8f" % (
                 self.state.numpy(),
-                self.state.numpy()[int(self.best_idx)],
-                max(self.best_ipc)
+                self.state.numpy()[int(best_idx)],
+                self.best_ipc
             )
             self.configs["logger"].info(msg)
         done = bool(
@@ -112,7 +116,7 @@ class RocketDesignEnv(BasicEnv):
             (torch.where(reward == 0)[0].shape == reward.shape)
         )
         self.n_step += 1
-        return self.state, reward, done
+        return self.state.clone(), reward.clone(), done
 
     def test_step(self, action):
         """
@@ -140,6 +144,7 @@ class RocketDesignEnv(BasicEnv):
         for i in range(self.configs["batch"]):
             if not self.design_space.validate(self.state[i].numpy()):
                 # no transition
+                self.configs["logger"].info("[INFO]: %s is invalid." % self.state[i].numpy())
                 dont_vlsi[i] = 1
                 reward[i] = 0
         # `valid_idx`: <torch.Tensor>
@@ -154,24 +159,27 @@ class RocketDesignEnv(BasicEnv):
             )
             k = 0
             for i in vlsi_idx:
-                reward[i] = ipc[k] - self.init_ipc[i]
+                reward[i] = ipc[k]
                 # update `self.best_ipc`
-                if reward[i] > 0:
-                    self.best_ipc[i] = ipc[k]
+                if reward[i] > self.ipc[i]:
+                    self.ipc[i] = ipc[k]
                 k += 1
         reward = torch.Tensor(reward)
-        msg = "[INFO]: reward: %s" % reward
+        msg = "[INFO]: state: %s, reward: %s, current best ipc: %s" % (self.state.numpy(), reward, self.ipc)
         self.configs["logger"].info(msg)
-        if not torch.equal(
-            self.best_idx,
-            torch.where(self.best_ipc == max(self.best_ipc))[0]
+        if bool(torch.lt(
+                self.best_ipc,
+                torch.max(self.ipc)
+            )
         ):
-            self.best_idx = torch.where(self.best_ipc == max(self.best_ipc))[0]
+            # the best IPC is increased
+            self.best_ipc = torch.max(self.ipc)
+            best_idx = torch.where(self.ipc == self.best_ipc)[0]
             self.last_update = self.n_step
-            msg = "[INFO]: current state: %s, best state: %s, best ipc: %.8f" % (
+            msg = "[INFO]: current state: %s, global best state: %s, global best ipc: %.8f" % (
                 self.state.numpy(),
-                self.state.numpy()[int(self.best_idx)],
-                max(self.best_ipc)
+                self.state.numpy()[int(best_idx)],
+                self.best_ipc
             )
             self.configs["logger"].info(msg)
         done = bool(
@@ -183,70 +191,87 @@ class RocketDesignEnv(BasicEnv):
             (torch.where(reward == 0)[0].shape == reward.shape)
         )
         self.n_step += 1
-        return self.state, reward, done
+        return self.state.clone(), reward.clone(), done
+
+    def get_next_state(self, reward):
+        """
+            re-generate a design if it is invalid
+        """
+        f = False
+        for i in range(reward.shape[0]):
+            if reward[i] == 0:
+                f = True
+                self.state[i] = self.design_space.sample_v3(
+                    1,
+                    self.design_space.bounds["decodeWidth"][::-1][i]
+                )
+        if f:
+            msg = "[INFO]: invalid state has been re-generated: %s" % self.state
+            self.configs["logger"].info(msg)
+        return self.state.clone()
 
     def re_init(self):
         self.configs["logger"].info("[INFO]: re-generate designs...")
         state = torch.Tensor()
-        for i in range(len(self.init_ipc)):
-            if self.init_ipc[i] == 0:
+        for i in range(len(self.ipc)):
+            if self.ipc[i] == 0:
                 state = torch.cat(
                     (
                         state,
-                        self.design_space.sample(1, int(self.state[i][4]))
+                        self.design_space.sample_v3(1, int(self.state[i][4]))
                     )
                 ).long()
         ipc = torch.Tensor(online_vlsi(self.configs, state.numpy()))
 
         # replace the current state
         k = 0
-        for i in range(len(self.init_ipc)):
-            if self.init_ipc[i] == 0:
+        for i in range(len(self.ipc)):
+            if self.ipc[i] == 0:
                 self.state[i] = state[k]
-                self.init_ipc[i] = ipc[k]
+                self.ipc[i] = ipc[k]
                 k += 1
 
     def test_re_init(self):
         self.configs["logger"].info("[INFO]: re-generate designs...")
         state = torch.Tensor()
-        for i in range(len(self.init_ipc)):
-            if self.init_ipc[i] == 0:
+        for i in range(len(self.ipc)):
+            if self.ipc[i] == 0:
                 state = torch.cat(
                     (
                         state,
-                        self.design_space.sample(1, int(self.state[i][4]))
+                        self.design_space.sample_v3(1, int(self.state[i][4]))
                     )
                 ).long()
         ipc = torch.Tensor(test_online_vlsi(self.configs, state.numpy()))
 
         # replace the current state
         k = 0
-        for i in range(len(self.init_ipc)):
-            if self.init_ipc[i] == 0:
+        for i in range(len(self.ipc)):
+            if self.ipc[i] == 0:
                 self.state[i] = state[k]
-                self.init_ipc[i] = ipc[k]
+                self.ipc[i] = ipc[k]
                 k += 1
 
     def reset(self):
-        self.state = self.design_space.sample(self.configs["batch"])
-        self.init_ipc = torch.Tensor(online_vlsi(self.configs, self.state.numpy()))
-        self.best_ipc = self.init_ipc.clone()
-        self.best_idx = torch.where(self.best_ipc == max(self.best_ipc))[0]
+        # Notice: `self.configs["batch"] // 5` is required
+        self.state = self.design_space.sample_v1(self.configs["batch"])
+        self.ipc = torch.Tensor(online_vlsi(self.configs, self.state.numpy()))
+        self.best_ipc = torch.max(self.ipc)
         # If some configs. cannot simulate, we need to re-initialize it
-        while not (torch.where(self.init_ipc == 0)[0].shape[0] == 0):
+        while not (torch.where(self.ipc == 0)[0].shape[0] == 0):
             self.re_init()
-        msg = "[INFO]: reset state: %s, reset ipc: %s" % (self.state, self.best_ipc)
+        msg = "[INFO]: reset state: %s, reset ipc: %s" % (self.state, self.ipc)
         self.configs["logger"].info(msg)
         return self.state.clone()
 
     def test_reset(self):
-        self.state = self.design_space.sample(self.configs["batch"])
-        self.init_ipc = torch.Tensor(test_online_vlsi(self.configs, self.state.numpy()))
-        self.best_ipc = self.init_ipc.clone()
-        self.best_idx = torch.where(self.best_ipc == max(self.best_ipc))[0]
+        # Notice: `self.configs["batch"] // 5` is required
+        self.state = self.design_space.sample_v1(self.configs["batch"])
+        self.ipc = torch.Tensor(test_online_vlsi(self.configs, self.state.numpy()))
+        self.best_ipc = torch.max(self.ipc)
         # If some configs. cannot simulate, we need to re-initialize it
-        while not (torch.where(self.init_ipc == 0)[0].shape[0] == 0):
+        while not (torch.where(self.ipc == 0)[0].shape[0] == 0):
             self.test_re_init()
-        msg = "[INFO]: reset state: %s, reset ipc: %s" % (self.state, self.best_ipc)
+        msg = "[INFO]: reset state: %s, reset ipc: %s" % (self.state, self.ipc)
         self.configs["logger"].info(msg)
         return self.state.clone()
