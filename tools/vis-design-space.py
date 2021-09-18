b8bad0ecc865e71cb2060775479e31606d7ec57d
Chen BAI <baichen318@gmail.com>
2021-09-18 20:11:33 +0800

    Add 3D visualization

---
+++ tools/vis-design-space.py
@@ -0,0 +1,52 @@
+# Author: baichen318@gmail.com
+
+import sys, os
+sys.path.insert(
+    0,
+    os.path.join(os.path.dirname(__file__), os.path.pardir, "dse")
+)
+sys.path.insert(
+    0,
+    os.path.join(os.path.dirname(__file__), os.path.pardir, "util")
+)
+sys.path.insert(
+    0,
+    os.path.join(os.path.dirname(__file__), os.path.pardir, "vlsi")
+)
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from util import load_txt, get_configs, parse_args
+
+def vis_design_space():
+    dataset = load_txt(os.path.join(os.path.pardir, configs["dataset-output-path"]), fmt=float)
+    remove_idx = []
+    perf = dataset[:, -3]
+    power = dataset[:, -2]
+    area = dataset[:, -1]
+    # remove invalid data
+    for i in range(perf.shape[0]):
+        if perf[i] == 0:
+            remove_idx.append(i)
+            continue
+        if power[i] == 0:
+            remove_idx.append(i)
+            continue
+    perf = np.delete(perf, remove_idx)
+    power = np.delete(power, remove_idx)
+    area = np.delete(area, remove_idx)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(perf, power, area)
+    path = os.path.join(os.path.dirname(__file__), "vis-%s.png" % configs["design"])
+    plt.savefig(
+        path,
+        dpi=600
+    )
+    print("[INFO]: save %s" % path)
+    plt.show()
+
+
+if __name__ == "__main__":
+    configs = get_configs(parse_args().configs)
+    vis_design_space()
