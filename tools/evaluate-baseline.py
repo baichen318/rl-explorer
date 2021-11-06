a12f0643081f4deaf7c176770d0a072f995dc5b2
Chen BAI <baichen318@gmail.com>
2021-11-06 20:34:13 +0800

    Fix a path setting in baseline evaluation

--- tools/evaluate-baseline.py
+++ tools/evaluate-baseline.py
@@ -3,7 +3,7 @@
 import sys, os
 sys.path.insert(
     0,
-    os.path.join(os.path.pardir, os.path.dirname(__file__))
+    os.path.join(os.path.dirname(__file__), os.path.pardir)
 )
 sys.path.insert(
     0,
